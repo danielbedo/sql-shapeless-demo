86c9a1c1ec1c96988a94456d2294a1dcd400f534
daniel.bedo <danielbedo@gmail.com>
2016-04-17 22:48:13 +0200

    first pass

--- build.sbt
+++ build.sbt
@@ -10,5 +10,8 @@ resolvers ++= Seq(
 libraryDependencies ++= Seq(
   "org.scalaz" %% "scalaz-core" % "7.1.3",
   "com.chuusai" %% "shapeless" % "2.2.5",
-  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+  "com.lihaoyi" % "ammonite-repl" % "0.5.7" % "test" cross CrossVersion.full
 )
+
+initialCommands in (Test, console) := """ammonite.repl.Main.run("")"""
--- src/main/scala/sqlops/generic/TestGeneration.scala
+++ src/main/scala/sqlops/generic/TestGeneration.scala
@@ -1,10 +1,49 @@
 package sqlops.generic
 
+import shapeless._
+import shapeless.ops.record._
+import shapeless.ops.hlist.{Mapper,ToTraversable}
+import shapeless.tag._
+
+object QueryGenerator {
+
+  object attrsPoly extends Poly1 {
+    implicit def atTaggedSymbol[T] = at[Symbol with Tagged[T]](_.name)
+  }
+
+  object valuesPoly extends Poly1 {
+    implicit def atInt = at[Int](_.toString)
+    implicit def atString = at[String](identity)
+    implicit def atBoolean = at[Boolean](_.toString)
+  }
+
+  def plainInsert[T, ClassRepr <: HList, KeysRepr <: HList, KeysMapperRepr <: HList, ValuesRepr <: HList, ValuesMapperRepr <: HList](x: T, tableName: String)(implicit
+    gen: LabelledGeneric.Aux[T, ClassRepr],
+    keys: Keys.Aux[ClassRepr, KeysRepr],
+    mapper: Mapper.Aux[attrsPoly.type, KeysRepr, KeysMapperRepr],
+    keysTraversable: ToTraversable.Aux[KeysMapperRepr, List, String],
+
+    values: Values.Aux[ClassRepr, ValuesRepr],
+    valuesMapper: Mapper.Aux[valuesPoly.type, ValuesRepr, ValuesMapperRepr],
+    valuesTraversable: ToTraversable.Aux[ValuesMapperRepr, List, String]) : String = {
+
+    val attributes = keys().map(attrsPoly).toList
+    val vals = values.apply(gen.to(x)).map(valuesPoly).toList
+
+    s"INSERT INTO $tableName (${attributes.mkString(",")}) VALUES (${vals.mkString(",")})"
+  }
+
+
+
+}
+
 object TestGeneration extends App {
 
-  case class Question(id: Int, title: String)
-  val q1 = Question(1, "Very first question")
+  import QueryGenerator._
 
-  // println(q1.insert)
+  case class Question(id: Int, title: String, strippedTitle: Boolean)
+  val q1 = Question(1, "Very first question", true)
+
+  println(plainInsert(q1, "question_table"))
 
 }
